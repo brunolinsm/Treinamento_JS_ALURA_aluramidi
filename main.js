d2901fc95937cf9ed2830015551d8b7ae5cce49a
Bruno Lins Maia <brunolinsm@gmail.com>
2024-02-02 19:21:05 -0300

    Apagados comentários e alterados nomes das funções e variáveis

--- main.js
+++ main.js
@@ -1,58 +1,23 @@
-function tocaSom(seletorAudio){
-    const elemento = document.querySelector(seletorAudio);
-
-    if (elemento != null && elemento.localName === 'audio'){
-        elemento.play();
-     }
-
-    else {
-        alert('Elemento não encontrado ou seletor inválido!');
-    }
-}
-
-// document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-// function tocaSomClap(){
-//     document.querySelector('#som_tecla_clap').play();
-// }
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-/* Opção de estrutura de repetição com While, código menos otimizado
+function tocaSom(seletorSom){
+    const somEscolhido = document.querySelector(seletorSom);
 
-let contador = 0;
-
-while(contador < listaDeTeclas.length){
-
-    const tecla = listaDeTeclas[contador]
-    const instrumento = tecla.classList[1];
-
-    // console.log(instrumento);
-
-    //Template string
-    const idAudio = `#som_${instrumento}`;
-
-    tecla.onclick = function (){
-        tocaSom(idAudio);
-    }
-    // listaDeTeclas[contador].onclick = tocaSom;
-
-    contador +=1;
-
-    // console.log(contador);
-
+    if (somEscolhido != null && somEscolhido.localName === 'audio'){
+     somEscolhido.play();
+     }
+    
+     alert('Elemento não encontrado ou seletor inválido!');
 }
-*/
 
-//Opção ao código acima de estrutura de repetição otimizada usando for
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;  //template string
+    const idSom = `#som_${instrumento}`;
 
     tecla.onclick = function(){
-        tocaSom(idAudio);
+        tocaSom(idSom);
     }
     
     tecla.onkeydown = function(evento){
